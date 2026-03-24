5b90bf2f0c0a5deb2820aea28ff0a06dd16dcfa3
The apprentice <luis.caputo@alunos.unic.co.ao>
2026-03-24 09:03:22 +0100

    Ceriado o Projrcto SomaVetores

---
+++ SomaVetores/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace SomaVetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+            int N, i;
+            double soma, media;
+
+            Console.Write("Quantos números vai digitar? ");
+            N = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[N];
+
+            for (i = 0; i < N; i++)
+            {
+                Console.Write("Digite um número: ");
+                vet[i] = double.Parse(Console.ReadLine(), CI);
+            }
+            Console.WriteLine();
+
+            Console.Write("VETORES = ");
+            for (i = 0; i < N; i++) 
+            {
+                Console.Write(vet[i].ToString("F2", CI) + " ");
+            }
+            Console.WriteLine();
+
+            soma = 0;
+            for (i = 0; i < N; i++)
+            {
+                soma = soma + vet[i];
+            }
+
+            Console.WriteLine("SOMA = " + soma.ToString("F2", CI));
+
+            media = soma / N;
+            Console.WriteLine("MEDIA = " + media.ToString("F2", CI));
+
+
+        }
+    }
+}
